e3fce858920cebe7d48b5535483dc12acbf3b9c9
Iamneo <techadmin@iamneo.ai>
2023-11-01 09:49:48 +0000

    Backup Commit

--- SGMS/Models/SGMSDbcontext.cs
+++ SGMS/Models/SGMSDbcontext.cs
@@ -3,3 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 namespace SGMS.Models;
 public class SGMSDbContext : DbContext
+{
+    public virtual DbSet<Student> Students{get;set;}
+    public virtual DbSet<Grade> Grades{get;set;}
+    public SGMSDbContext(){}
+    public SGMSDbContext(DbContextOptions<SGMSDbContext>options):base (options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+    {
+        if(!optionBuilder.IsConfigured)
+        {
+            optionBuilder.UseSqlServer("User ID=sa; password=examlyMssql@123; server=localhost;Database=SGMSDb;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+
+        }
+    }
+
+}
