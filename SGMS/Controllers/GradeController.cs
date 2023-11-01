0dbed1384c85f82559f4f683f7ce4307107c2a5d
Iamneo <techadmin@iamneo.ai>
2023-11-01 10:04:47 +0000

    Backup Commit

---
+++ SGMS/Controllers/GradeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using SGMS.Models;
+namespace SGMS.Controllers;
+public class GradeController:Controller
+{
+    private readonly SGMSDbContext context;
+    public GradeController(SGMSDbContext_context)
+    {
+        context = _conte
+    }
+
+}
