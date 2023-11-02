03929c3efe19be9de7ad3131cc9cfaaf0bb96856
Iamneo <techadmin@iamneo.ai>
2023-11-02 04:14:14 +0000

    Backup Commit

--- Practice/Models/PDbContext.cs
+++ Practice/Models/PDbContext.cs
@@ -1,2 +1,19 @@
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkC
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace Practice.Models;
+public class PDbContext : DbContext
+{
+    public PDbContext(){}
+    public virtual DbSet<Practice> Practice{get;set;}
+    public PDbContext(DbContextOptions<PDbContext>options):base(options)
+    {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("User ID=sa; password=examlyMssql@123; server=localhost;Database=EmsDb;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+        }
+    }
+}
