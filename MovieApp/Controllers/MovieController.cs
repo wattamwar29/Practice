d3c1ecff66e917b0cbb9c75e87b5da9b7333a451
Iamneo <techadmin@iamneo.ai>
2023-11-04 07:13:03 +0000

    Backup Commit

--- MovieApp/Controllers/MovieController.cs
+++ MovieApp/Controllers/MovieController.cs
@@ -33,5 +33,23 @@ namespace MovieApp.Controllers
            }
            return Ok(data);
         }
+        [HttpPost]
+        [Route("AddMovie")]
+        public IActionResult Post(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    context.Movies.Add(movie);
+                    context.SaveChanges();
+                }
+                catch(System.Exception ex)
+                {
+                    return BadRequest(ex.InnerException.Message);
+                }               
+            }
+            return Created("Record Added",movie);
+        }
     }
 }
