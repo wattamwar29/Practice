292b4ad41c72d3b31b3d36e74b5306ec944bc034
Iamneo <techadmin@iamneo.ai>
2023-11-04 05:43:03 +0000

    Backup Commit

--- MovieApp/Models/MovieContext.cs
+++ MovieApp/Models/MovieContext.cs
@@ -1,5 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 namespace MovieApp.Models
 {
-    public class MovieContext:Db
+    public class MovieContext:DbContext
+    {
+        public virtual DbSet<Movie> Movies{get;set;}
+        public virtual DbSet<Detail> Details{get;set;}
+        public MovieContext(){}
+        public MovieContext(DbContextOptions<MovieContext>options):base(options)
+        {}
+        protected override void OnConfiguring(DbContextOptionBuilder optionBuilder)
+        {
+            
+        }
+    }
 }
