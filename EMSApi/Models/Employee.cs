1b638fdfcc76abde50fd651c6624b5a4d75d04aa
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:21:52 +0000

    Backup Commit

---
+++ EMSApi/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMSApi.Models;
+
+public partial class Employee
+{
+    public int EmpId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Salary { get; set; }
+
+    public int DeptId { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public DateTime Dob { get; set; }
+
+    public virtual Department Dept { get; set; } = null!;
+}
---
+++ EMSApi/Models/EmsDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMSApi.Models;
+
+public partial class EmsDbContext : DbContext
+{
+    public EmsDbContext()
+    {
+    }
+
+    public EmsDbContext(DbContextOptions<EmsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123; server=localhost;Database=EmsDb;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.ToTable("Department");
+
+            entity.Property(e => e.DeptName).HasMaxLength(30);
+            entity.Property(e => e.Location)
+                .HasMaxLength(30)
+                .HasColumnName("location");
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmpId);
+
+            entity.HasIndex(e => e.DeptId, "IX_Employees_DeptId");
+
+            entity.HasIndex(e => e.Email, "IX_Employees_Email").IsUnique();
+
+            entity.Property(e => e.Dob).HasColumnName("DOB");
+            entity.Property(e => e.Email).HasMaxLength(40);
+            entity.Property(e => e.Name).HasMaxLength(30);
+
+            entity.HasOne(d => d.Dept).WithMany(p => p.Employees).HasForeignKey(d => d.DeptId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
