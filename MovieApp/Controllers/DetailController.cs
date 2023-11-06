05218834ea82282faf84f49801d7ae8b6fac6f58
Iamneo <techadmin@iamneo.ai>
2023-11-06 05:00:39 +0000

    Backup Commit

--- MovieApp/Controllers/DetailController.cs
+++ MovieApp/Controllers/DetailController.cs
@@ -22,7 +22,74 @@ namespace MovieApp.Controllers
         [Route("ListDetail")]
         public IActionResult Get()
         {
-            
+            var data=from d in context.Details select new{MovieName=d.Movie.Name,Artist=d.Actor};
+            return Ok(data);
         }
+        [HttpGet]
+    [Route("ListDetails/{id}")]
+    public IActionResult Get(int? id)
+    {
+       if(id==null)
+        {
+            return BadRequest("Id cannot be null");
+        }
+            var data = (from d in context.Details where d.MovieId ==id select d).FirstOrDefault();
+            if(id==null)
+            {
+                return NotFound($"Detail {id} not found");
+            }
+            return Ok(data);
+        }
+         [HttpPost]
+        [Route("AddDetails")]
+        public IActionResult Post([FromBody] Detail detail)
+        {
+            if(ModelState.IsValid){
+                try{
+                    context.Details.Add(detail);
+                    context.SaveChanges();
+                }
+                catch(System.Exception ex){
+                    return BadRequest(ex.InnerException.Message);
+                }
+            }
+            return Created("Record Added",detail);
+        }
+ 
+        [HttpPut]
+        [Route("EditDetails/id")]
+        public IActionResult Put(int id, Detail detail)
+        {
+            if(ModelState.IsValid)
+            {Detail detail1 = context.Details.Find(id);
+            detail1.DetailId = detail.DetailId;
+            detail1.Actor=detail.Actor;
+            detail1.MovieId=detail.MovieId;
+            detail1.Gender=detail.Gender;
+            detail1.Role=detail.Gender;
+            context.SaveChanges();
+            return Ok();
+            }  
+           
+            return BadRequest("Unable to Edit record");
+        }
+        // [HttpDelete]
+        // [Route("DeleteMovies/id")]
+        // public IActionResult Delete(int id)
+        // {
+        //     try{
+        //             var movie = context.Movies.Where(m=>m.DetailId==id);
+        //             if(movie.Count()!=0){
+        //                 throw new Exception("Cannot Delete Movie");
+        //             }
+        //             var data = context.Details.Find(id);
+        //             context.Details.Remove(data);
+        //             return Ok();
+        //         }
+        //         catch(System.Exception ex){
+        //             return BadRequest(ex.Message);
+        //         }
+        // }
+ 
     }
 }
