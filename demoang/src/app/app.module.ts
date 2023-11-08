17120348d1ea3d7c01efccbf3eac6ecedd32110b
Iamneo <techadmin@iamneo.ai>
2023-11-08 10:11:34 +0000

    Backup Commit

--- demoang/src/app/app.module.ts
+++ demoang/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleComponent } from './simple/simple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponent
   ],
   imports: [
     BrowserModule,
