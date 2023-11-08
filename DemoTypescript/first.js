a6686bdb748810ef64defc8f7d3f578b82f319cf
Iamneo <techadmin@iamneo.ai>
2023-11-08 05:18:40 +0000

    Backup Commit

--- DemoTypescript/first.js
+++ DemoTypescript/first.js
@@ -9,3 +9,15 @@ var csno = '40';
 // var result=fno+csno+sno
 var result = fno + sno + csno;
 console.log(result);
+var fruits = ['Apple', 'Mango', 'Pear', 'Orange', 'Chikoo'];
+for (var i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+function sayHello() {
+    console.log('Hello World');
+}
+function multiply(a, b) {
+    return a * b;
+}
+sayHello();
+console.log(multiply(4, 5));
--- DemoTypescript/first.ts
+++ DemoTypescript/first.ts
@@ -9,3 +9,18 @@ var csno:string='40'
 // var result=fno+csno+sno
 var result=fno+sno+csno
 console.log(result)
+var fruits:string[]=['Apple','Mango','Pear','Orange','Chikoo']
+for(var i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i])
+}
+function sayHello():void
+{
+    console.log('Hello World')
+}
+function multiply(a:number,b:number):number
+{
+    return a*b
+}
+sayHello()
+console.log(multiply(4,5))
