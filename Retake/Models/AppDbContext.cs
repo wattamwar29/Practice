4348959ef5e2270493c703e22b4771467685b7b3
Iamneo <techadmin@iamneo.ai>
2023-11-10 11:34:58 +0000

    Backup Commit

--- Retake/Models/AppDbContext.cs
+++ Retake/Models/AppDbContext.cs
@@ -1,10 +1,13 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.AspNetCore.Mvc;
 namespace Retake.Models;
 public class AppDbContext : DbContext
 {
     public virtual DbSet<Books>Books{get;set;}
     public AppDbContext(){}
-    public AppDbContext(DbContext)
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {}
+    protected override void 
 }
