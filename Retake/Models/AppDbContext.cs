7cdc247e067c854e2b6d8f3f848281b21038bf45
Iamneo <techadmin@iamneo.ai>
2023-11-10 11:04:58 +0000

    Backup Commit

--- Retake/Models/AppDbContext.cs
+++ Retake/Models/AppDbContext.cs
@@ -2,4 +2,9 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 namespace Retake.Models;
-public class AppDbContext
+public class AppDbContext : DbContext
+{
+    public virtual DbSet<Books>Books{get;set;}
+    public AppDbContext(){}
+    public AppDbContext(DbContext)
+}
