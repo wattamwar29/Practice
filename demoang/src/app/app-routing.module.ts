f0c921f9adddc8de9fd5af3244052a7afa5271ed
Iamneo <techadmin@iamneo.ai>
2023-11-13 10:22:34 +0000

    Backup Commit

--- demoang/src/app/app-routing.module.ts
+++ demoang/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
+import { EmpComponent } from './emp/emp.component';
+import { RegisterComponent } from './register/register.component';
+import { SimpleComponent } from './simple/simple.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'demo',component:DemoComponent},
+  {path:'emp',component:EmpComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'simple',component:SimpleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
