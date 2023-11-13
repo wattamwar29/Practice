9910bf7e200b5d200cfbdce7b0503399460e217d
Iamneo <techadmin@iamneo.ai>
2023-11-13 10:07:33 +0000

    Backup Commit

--- demoang/src/app/emp/emp.component.ts
+++ demoang/src/app/emp/emp.component.ts
@@ -1,32 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { FormGroup,FormControl,Validator } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-emp',
   templateUrl: './emp.component.html',
   styleUrls: ['./emp.component.css']
 })
 export class EmpComponent implements OnInit {
+ 
+ 
   constructor(private formbuilder:FormBuilder) { }
   empform=this.formbuilder.group({
-    name:['',[Validators.required,Validators.maxLength(30)]],
-    email:['',Validators.email],
-    city:['',Validators.pattern("[a-zA-Z")]
+  name:['',[Validators.required,Validators.maxLength(30)]],
+  email:['',Validators.email],
+  city:['',Validators.pattern("[a-zA-Z]")]
   })
-  Save(){
+  Save():void{
     console.log(this.empform.value)
   }
+ 
   // empform=this.formbuilder.group({
   //   firstname:[''],
-  //   age:[''],
-  //   address:this.formbuilder.group({
-  //     line1:[''],
-  //     city:[''],
-  //     state:['']
-  //   })
-  //})
-
+  // age:[''],
+  // address:this.formbuilder.group({
+  //   line1:[''],
+  //   city:[''],
+  //   state:['']
+  // })
+  // })
+ 
   ngOnInit() {
   }
-
+ 
 }
