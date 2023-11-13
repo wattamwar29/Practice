b65d11b986246049a791efef10448f84cab5aaf9
Iamneo <techadmin@iamneo.ai>
2023-11-13 09:07:33 +0000

    Backup Commit

--- demoang/src/app/app.module.ts
+++ demoang/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleComponent } from './simple/simple.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
+import{E}
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule
