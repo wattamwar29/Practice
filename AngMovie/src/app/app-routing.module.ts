9ad5fa46944be93d2b642ef7b23b94aa9d216e92
Iamneo <techadmin@iamneo.ai>
2023-11-15 11:24:30 +0000

    Backup Commit

--- AngMovie/src/app/app-routing.module.ts
+++ AngMovie/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListMovieComponent } from './list-movie/list-movie.component';
+import { FindmovieComponent } from './findmovie/findmovie.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listmovies',component:ListMovieComponent},
+  {path:'find/:id',component:FindmovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngMovie/src/app/app.module.ts
+++ AngMovie/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListMovieComponent } from './list-movie/list-movie.component';
 import { FindmovieComponent } from './findmovie/findmovie.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListMovieComponent,
-    FindmovieComponent
+    FindmovieComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
