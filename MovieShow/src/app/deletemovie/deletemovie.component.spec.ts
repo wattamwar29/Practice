4e9c16021ac2e53c20402714c38379db396f752d
Iamneo <techadmin@iamneo.ai>
2023-11-16 12:58:06 +0000

    Backup Commit

--- MovieShow/src/app/deletemovie/deletemovie.component.spec.ts
+++ MovieShow/src/app/deletemovie/deletemovie.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DeletemovieComponent } from './deletemovie.component';
+import { DeletemovieComponent } from '../deletemovie.component';
 
 describe('DeletemovieComponent', () => {
   let component: DeletemovieComponent;
--- MovieShow/src/app/deletemovie/deletemovie.component.ts
+++ MovieShow/src/app/deletemovie/deletemovie.component.ts
@@ -1,34 +1,34 @@
-// import { Component, OnInit } from '@angular/core';
-// import { MovieserviceService } from '../service/movieservice.service';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { IMovie } from '../model/imovie';
+import { Component, OnInit } from '@angular/core';
+import { MovieserviceService } from '../service/movieservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IMovie } from '../model/imovie';
 
-// @Component({
-//   selector: 'app-deletemovie',
-//   templateUrl: './deletemovie.component.html',
-//   styleUrls: ['./deletemovie.component.css']
-// })
-// export class DeletemovieComponent implements OnInit {
+@Component({
+  selector: 'app-deletemovie',
+  templateUrl: './deletemovie.component.html',
+  styleUrls: ['./deletemovie.component.css']
+})
+export class DeletemovieComponent implements OnInit {
 
-//   constructor(private ms:MovieserviceService,private route:Router,private ar: ActivatedRoute) { }
-//   moviedata: IMovie={id:0,name:'',yearrelease:0,rating:0}
-//   id:number
+  constructor(private ms:MovieserviceService,private route:Router,private ar: ActivatedRoute) { }
+  moviedata: IMovie={id:0,name:'',yearrelease:0,rating:0}
+  id:number
 
-//   ngOnInit() {
-//     const tid=this.ar.snapshot.paramMap.get('id')
-//     this.id=Number(tid)
-//     this.getMovie(this.id)
-//   }
-//   getMovie(id:number){
-//         this.ms.getMovie(id).subscribe((data:IMovie)=>this.moviedata=data)
-//       }
-//       saveData(movie:IMovie)
-//       {
-//         this.moviedata=movie
-//         this.ms.deleteMovie(this.moviedata).subscribe(()=>{
-//           alert("Record Deleted")
-//           this.route.navigate(['/listmovies'])
-//         })
-//       }
+  ngOnInit() {
+    const tid=this.ar.snapshot.paramMap.get('id')
+    this.id=Number(tid)
+    this.getMovie(this.id)
+  }
+  getMovie(id:number){
+        this.ms.getMovie(id).subscribe((data:IMovie)=>this.moviedata=data)
+      }
+      saveData(movie:IMovie)
+      {
+        this.moviedata=movie
+        this.ms.deleteMovie(this.moviedata).subscribe(()=>{
+          alert("Record Deleted")
+          this.route.navigate(['/listmovies'])
+        })
+      }
 
-//     }
+    }
