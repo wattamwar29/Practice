e59d0bf8e347aa26ed820e37ee13f09c80cd4f81
Iamneo <techadmin@iamneo.ai>
2023-11-16 12:28:06 +0000

    Backup Commit

--- MovieShow/src/app/deletemovie/deletemovie.component.ts
+++ MovieShow/src/app/deletemovie/deletemovie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieserviceService } from '../service/movieservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IMovie } from '../model/imovie';
 
 @Component({
   selector: 'app-deletemovie',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeletemovieComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ms:MovieserviceService,private route:Router,private ar: ActivatedRoute) { }
+  moviedata: IMovie={id:0,name:'',yearrelease:0,rating:0}
+  id:number
 
   ngOnInit() {
+    const tid=this.ar.snapshot.paramMap.get('id')
+    this.id=Number(tid)
+    this.getMovie(this.id)
   }
+  getMovie(id:number){
+        this.ms.getMovie(id).subscribe((data:IMovie)=>this.moviedata=data)
+      }
+      saveData(movie:IMovie)
+      {
+        this.moviedata=movie
+        this.ms.deleteMovie(this.moviedata).subscribe(()=>{
+          alert("Record Deleted")
+          this.route.navigate(['/listmovies'])
+        })
+      }
 
-}
+    }
