17b829750abbf0547cabcbd686f64e8354e512df
Iamneo <techadmin@iamneo.ai>
2023-11-17 09:43:53 +0000

    Backup Commit

--- jwt/Program.cs
+++ jwt/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModels.Tokens;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +11,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(opt=>{
     opt.DefaultAuthenticateScheme= JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme= JwrBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
 })
-.AddJwtBearer(option=>
+.AddJwtBearer(options=>
 {
-    
-})
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "http://0.0.0.0:8080",
+        ValidAudience = "http://0.0.0.0:8080",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+        ("superSecurityKey@345"))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
