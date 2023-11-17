fe35db522695f3ab2693ac85fa945138ab6f70b2
Iamneo <techadmin@iamneo.ai>
2023-11-17 06:28:54 +0000

    Backup Commit

--- MovieShow/src/app/reactform/reactform.component.ts
+++ MovieShow/src/app/reactform/reactform.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,Validator, Validators } from '@angular/forms';
+import { IMovie } from '../model/imovie';
 @Component({
   selector: 'app-reactform',
   templateUrl: './reactform.component.html',
   styleUrls: ['./reactform.component.css']
 })
 export class ReactformComponent implements OnInit {
-
-  constructor() { }
+  moviedata: IMovie
+  constructor(private fb:FormBuilder) { }
+  movieform =this.fb.group({
+    name:['',Validators.required],
+    yearrelease : ['',[Validators.min(2000),Validators.max(2023)]],
+    rating: ['',[Validators.required,Validators.min(1),Validators.max(5)]]
+  })
+  saveData(): void{
+    this.moviedata=this.movieform.value
+    if (this.moviedata.rating>5){
+      alert('Error in ratings')
+      return
+    }
+    console.log(this.moviedata)
+  }
 
   ngOnInit() {
   }
