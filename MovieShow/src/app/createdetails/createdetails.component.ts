41977e8ffcb965aeef35398d1680d14fee375494
Iamneo <techadmin@iamneo.ai>
2023-11-18 04:27:38 +0000

    Backup Commit

--- MovieShow/src/app/createdetails/createdetails.component.ts
+++ MovieShow/src/app/createdetails/createdetails.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Idetails } from '../model/idetails';
+import { MovieserviceService } from '../service/movieservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-createdetails',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createdetails.component.css']
 })
 export class CreatedetailsComponent implements OnInit {
+  detailsdata:Idetails = {detailid:0,actor:'',movieid:0,gender:'',role:''}
+  showdata: any[]=[]
 
-  constructor() { }
+  constructor(private ms: MovieserviceService,private router : Router) {
+
+   }
+   saveData(data:Idetails):void{
+    this.ms.addDetails(this.detailsdata).subscribe{
+      ()=>{
+        alert('Record Added Successfully')
+        this.router.navigate(['/listmovies'])
+      }
+    }
+   }
 
   ngOnInit() {
+    this.ms.getAllMovies().subscribe(data=>{this.showdata.push(...data)})
   }
 
 }
