8faac7926ccad29a9b056be1bebfda268dbb84f0
Iamneo <techadmin@iamneo.ai>
2023-11-18 05:57:34 +0000

    Backup Commit

--- MovieShow/src/app/service/movieservice.service.ts
+++ MovieShow/src/app/service/movieservice.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import {IMovie} from '../model/imovie';
-import {HttpClient,HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient,HttpErrorResponse,HttpHeaders} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Idetails } from '../model/idetails';
+import { HttpResponse } from '@angular/common/http';
+import {catchError}
 @Injectable({
   providedIn: 'root'
 })
@@ -16,21 +18,27 @@ export class MovieserviceService {
   }
   getMovie(id:number):Observable<IMovie>
   {
-    return this.httpclient.get<IMovie>(this.url+'/ListMovies/'+ id)
+    return this.httpclient.get<IMovie>(this.url+'/ListMovies/'+ id).pipe(catchError(this.handleError));
   }
   httpOptions={headers:new HttpHeaders({'Content-type':'application/json'})}
   addMovie(moviedata:IMovie):Observable<IMovie>{
-    return this.httpclient.post<IMovie>(this.url+'/AddMovie',moviedata,this.httpOptions)
+    return this.httpclient.post<IMovie>(this.url+'/AddMovie',moviedata,this.httpOptions).pipe(catchError(this.handleError));
   }
   editMovie(moviedata:IMovie):Observable<IMovie>{
-    return this.httpclient.put<IMovie>(this.url +'/EditMovie/'+moviedata.id,moviedata,this.httpOptions)
+    return this.httpclient.put<IMovie>(this.url +'/EditMovie/'+moviedata.id,moviedata,this.httpOptions).pipe(catchError(this.handleError));
 }
 deleteMovie(id:number):Observable<IMovie>
 {
-  return this.httpclient.delete<IMovie>(this.url+'/DeleteMovie/'+id)
+  return this.httpclient.delete<IMovie>(this.url+'/DeleteMovie/'+id).pipe(catchError(this.handleError));
 }
 addDetails(detailsdata: Idetails) :Observable<Idetails>{
   return this.httpclient.post<Idetails>(this.url2 + '/AddDetails',detailsdata, this.httpOptions)
+}
+handleError(error:HttpErrorResponse)
+{
+  var errmsg=error.status+'\n' + error.statusText + '\n' + error.error
+  alert(errmsg)
+  return throwError(errmsg)
 }
 
 }
